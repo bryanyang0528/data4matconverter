d1e2d59b0f76d9adf14f3952cc8270bb7621162a
Bryan <kenshin2004528@gmail.com>
2018-03-28 16:58:17 +0800

    refactor

--- src/converter/core.py
+++ src/converter/core.py
@@ -5,18 +5,25 @@ from pyspark.sql import SparkSession
 
 class Converter(object):
 
-    def __init__(self, src_format, src_path, **kwargs):
+    def __init__(self, src_format=None, src_path=None, **kwargs):
         self.spark = SparkSession.builder.getOrCreate()
-        self._df = self.read(src_format=src_format, path=src_path, **kwargs)
+        self._df = None
+        if src_format and src_path:
+            self.read(src_format=src_format, path=src_path, **kwargs)
 
     @property
     def df(self):
         return self._df
 
+    @df.setter
+    def df(self, value):
+        self._df = value
+
     def read(self, src_format, path, **kwargs):
-        self._df = self.spark.read.format(src_format).load(path=path, **kwargs)
+        self.df = self.spark.read.format(src_format).load(path=path, **kwargs)
         return self
 
-    def write(self, dest_format, path, **kwargs):
-        self._df.write.format(dest_format).save(path=path, **kwargs)
+    def write(self, format, path, **kwargs):
+        if self.df:
+            self.df.write.format(format).save(path=path, **kwargs)
         return self
